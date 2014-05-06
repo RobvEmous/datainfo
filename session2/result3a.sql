021cd65ad5bf5ba9b7eb3e9dca6c8c51901d7c33
Rob van Emous <robvemous@gmail.com>
2014-05-06 16:39:37 +0200

    Session 1 - result3a en result 3b
    result3a update

--- session2/result3a.sql
+++ session2/result3a.sql
@@ -1,19 +1,19 @@
-"Godfather, The";1972
-"Shawshank Redemption, The";1994
-"Schindler's List";1993
-"Casablanca";1942
-"Citizen Kane";1941
-"American Beauty";1999
-"Godfather: Part II, The";1974
-"Star Wars";1977
-"Shichinin no samurai";1954
-"One Flew Over the Cuckoo's Nest";1975
-"Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb";1964
-"Rear Window";1954
-"Usual Suspects, The";1995
-"Paths of Glory";1957
-"M";1931
-"Almost Famous";2000
-"General, The";1927
-"Ladri di biciclette";1948
-"Ikiru";1952
+﻿CREATE TABLE R (
+A INT, 
+B INT, 
+C INT, 
+D INT, 
+E INT, 
+F INT
+);
+
+CREATE TABLE F (
+x VARCHAR(10), 
+y VARCHAR(10),
+violation BOOLEAN, 
+motivation VARCHAR(200)
+);
+
+INSERT INTO F VALUES (
+
+)
