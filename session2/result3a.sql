92db7134aeed45c35427884340758ddfee8816d8
Rob van Emous <robvemous@gmail.com>
2014-05-06 16:32:34 +0200

    Session 1 - result3a

---
+++ session2/result3a.sql
@@ -0,0 +1,19 @@
+"Godfather, The";1972
+"Shawshank Redemption, The";1994
+"Schindler's List";1993
+"Casablanca";1942
+"Citizen Kane";1941
+"American Beauty";1999
+"Godfather: Part II, The";1974
+"Star Wars";1977
+"Shichinin no samurai";1954
+"One Flew Over the Cuckoo's Nest";1975
+"Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb";1964
+"Rear Window";1954
+"Usual Suspects, The";1995
+"Paths of Glory";1957
+"M";1931
+"Almost Famous";2000
+"General, The";1927
+"Ladri di biciclette";1948
+"Ikiru";1952
