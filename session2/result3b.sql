263527647667d56d5da51df01502f99975597fc2
Rob van Emous <robvemous@gmail.com>
2014-05-06 16:46:23 +0200

    First commit v2 - result3b

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,14 +1,4 @@
 SELECT p.pid FROM Person p 
-WHERE 'p is an actor that played in "Back to the Future"'; 
-
-SELECT p.pid FROM Person p 
-WHERE EXISTS( 
-SELECT * FROM Acts a –­ there exists an actor that is p
-WHERE a.pid = p.pid 
-AND 'a played in "Back to the Future"' 
-); 
-
-SELECT p.pid FROM Person p 
 WHERE EXISTS( 
 SELECT * FROM Acts a 
 WHERE a.pid = p.pid 
