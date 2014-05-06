74f295c3052558c4b568b6a4f044ed76e31ebfca
Rob van Emous <robvemous@gmail.com>
2014-05-06 16:25:25 +0200

    Session 1
    v2

---
+++ session2/excercise1.sql
@@ -0,0 +1,8 @@
+INSERT INTO problem1 (task, result, motivation) VALUES ('a', true, 'als je de capaciteit van een voertuig weet, volgt daar nog niet meteen één voertuig uit (maar meerdere)');
+INSERT INTO problem1 (task, result, motivation) VALUES ('d', true, 'als je weet met welk pakje je te maken hebt kun je via de factuur het afleveradres opzoeken');
+INSERT INTO problem1 (task, result, motivation) VALUES ('b', true, 'als je het voertuig weet weet je ook de capaciteit van het voertuig, dit is duidelijk');
+INSERT INTO problem1 (task, result, motivation) VALUES ('c', false, 'als je A en F weet is alleen duidelijk dat je te maken hebt met een pakje voor dat adres, maar niet één bepaald pakje');
+INSERT INTO problem1 (task, result, motivation) VALUES ('e', true, 'als je weet met welk pakje je te maken hebt weet je ook de ontvanger en de prijs van het pakje');
+INSERT INTO problem1 (task, result, motivation) VALUES ('f', false, 'B kan gerechtigd zijn voor meerdere V''s dus uit B volgt niet één V (meerdere)');
+INSERT INTO problem1 (task, result, motivation) VALUES ('g', false, 'B kan gerechtigd zijn voor meerdere V''s dus uit een V (en C) volgt niet één bestuurder');
+
