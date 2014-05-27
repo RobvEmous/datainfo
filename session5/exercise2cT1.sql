30a3185b9bf2b961aa4c5077eeced18a731be7ae
Rob van Emous <robvemous@gmail.com>
2014-05-27 15:26:05 +0200

    Session 5
    v2

---
+++ session5/exercise2cT1.sql
@@ -0,0 +1,13 @@
+﻿BEGIN; -- read uncommited (lvl 1)
+--read1(y)
+SELECT titel FROM Boek WHERE isbn = '0321228383';
+--write1(y)
+UPDATE Boek SET titel = titel || ' deel 1' WHERE isbn = '0321228383';
+END;
+
+BEGIN; -- read commited (lvl 2)
+--read1(x)
+SELECT titel FROM Boek WHERE isbn = '0136067018';
+--write1(x)
+UPDATE Boek SET titel = titel || ' deel 1' WHERE isbn = '0136067018';
+END;
---
+++ session5/exercise2cT2.sql
@@ -0,0 +1,7 @@
+﻿BEGIN; -- read commited (lvl 2)
+--read2(x) <- dirty read
+SELECT titel FROM Boek WHERE isbn = ' 0136067018';
+--write2(x)
+UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = ' 0136067018';
+--T2 END/ROLLBACK
+END;
